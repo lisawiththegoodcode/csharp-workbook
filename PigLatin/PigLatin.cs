4dfe39e619db55487c1fb8fec0764831d2dda2e8
Lisa Umhey <lisa@revroux.local>
2018-07-16 20:39:10 -0500

    first commit for pig latin

--- PigLatin/PigLatin.cs
+++ PigLatin/PigLatin.cs
@@ -7,11 +7,15 @@ namespace PigLatin
         public static void Main()
         {
             // your code goes here
-
+            
             // leave this command at the end so your program does not close automatically
             Console.ReadLine();
         }
         
+        public static void ReadWord(string word)
+        {
+ 
+        }
         public static string TranslateWord(string word)
         {
             // your code goes here
