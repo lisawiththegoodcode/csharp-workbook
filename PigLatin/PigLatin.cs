6f6f007405777169988f795c6be740538df53dc9
lisawiththegoodcode <lisa.umhey@gmail.com>
2018-07-17 11:08:01 -0500

    end of class commit

--- PigLatin/PigLatin.cs
+++ PigLatin/PigLatin.cs
@@ -7,14 +7,24 @@ namespace PigLatin
         public static void Main()
         {
             // your code goes here
-            
+            Console.WriteLine("What word would you like to translate to Pig Latin?");
+            ReadWord(word);
             // leave this command at the end so your program does not close automatically
-            Console.ReadLine();
+            string word = Console.ReadLine();
         }
         
         public static void ReadWord(string word)
         {
- 
+            for (int i = 0; i <= word.Length; i++)
+            {
+                if word[i] == 'a';
+                {
+                    return i;
+                }
+                else if word[i] == 'e';
+
+                //index of any is doing two loops inside of another
+            }
         }
         public static string TranslateWord(string word)
         {
