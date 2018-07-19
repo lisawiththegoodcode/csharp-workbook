d97fa1d11640adb6720e350171f82176d9ee9018
lisawiththegoodcode <lisa.umhey@gmail.com>
2018-07-19 12:25:44 -0500

    Rock Paper Scissors complete

--- RockPaperScissors/RockPaperScissors.cs
+++ RockPaperScissors/RockPaperScissors.cs
@@ -4,75 +4,25 @@ namespace RockPaperScissors
 {
     class Program
     {
-        public static void Scoreboard(int yourScore, int computerScore)
-        {
-            yourScore += yourScore;
-            computerScore += computerScore; 
-            Console.WriteLine($"Current Score: Player 1-{yourScore} / Computer-{computerScore}");
-            GameLoop();
-
-        }
-
-        public static void Scorekeeper(int winner)
-        {
-            int score1 = 0;
-            int score2 = 0;
-
-            if (winner == 1)
-            {
-                score1++;
-                Console.WriteLine("Score it now {0}", score1);
-
-            }
-            else if (winner == 2)
-            {
-                score2++;
-                Console.WriteLine("Score it now {0}", score2);
-            }
-            Scoreboard(score1, score2);
-        }
-        public static void GameLoop()
+        public static void GameLoop(int winner, int yourScore, int computerScore)
         {
             Console.WriteLine("Do you want to play again? [y/n]");
             string answer = Console.ReadLine();
             if ((answer == "y") || (answer =="Y") || (answer =="Yes") || (answer =="YES") || (answer =="yes"))
             {
-                GamePlay();
+                GamePlay(winner, yourScore, computerScore);
             } 
             else
             {
-                 Console.WriteLine("Thank you for playing!");
+                 Console.WriteLine("Thank you for playing. Goodbye!");
             }
         } 
-        public static void CheckForWins(string hand1, string hand2)
+        public static void GamePlay(int winner, int yourScore, int computerScore)
         {
-            int winner = 0;
-            //compare the two hands
-            if ((hand1 == "rock" && hand2 == "scissors") || (hand1 == "paper" && hand2 == "rock") || (hand1 == "scissors" && hand2 == "paper"))
-            {
-                winner = 1;
-                Console.WriteLine($"Player {winner} wins!");
-
-            }
-            else if (hand1 == hand2)
-            {
-                Console.WriteLine("It's a draw!");
-            }
-            else if (((hand2 == "rock" && hand1 == "scissors") || (hand2 == "paper" && hand1 == "rock") || (hand2 == "scissors" && hand1 == "paper")))
-            {
-                winner = 2;
-                Console.WriteLine("Computer wins!");
-            } 
-            else    
-            {
-                Console.WriteLine("You did not enter a valid hand, please try again.");
-            }
-            Scorekeeper(winner);
-        }
-        public static void GamePlay()
-        {
-            Console.WriteLine("Enter hand for player 1: [rock/paper/scissors]");
+            Console.WriteLine();
+            Console.WriteLine("Enter hand for Player 1: [rock/paper/scissors]");
             string hand1 = Console.ReadLine().ToLower();
+            Console.WriteLine();
             Console.WriteLine("Computer plays:");
             
             //randomly generate and output rock paper or scisoors
@@ -92,20 +42,62 @@ namespace RockPaperScissors
                 hand2 = "scissors";
             }
             Console.WriteLine(hand2);
-            CheckForWins(hand1, hand2);
+            Console.WriteLine();
+
+            //compares hands to check for wins
+            if ((hand1 == "rock" && hand2 == "scissors") || (hand1 == "paper" && hand2 == "rock") || (hand1 == "scissors" && hand2 == "paper"))
+            {
+                winner = 1;
+                Console.WriteLine($"Player {winner} wins!");
+
+            }
+            else if (hand1 == hand2)
+            {
+                winner = 0;
+                Console.WriteLine("It's a draw!");
+            }
+            else if (((hand2 == "rock" && hand1 == "scissors") || (hand2 == "paper" && hand1 == "rock") || (hand2 == "scissors" && hand1 == "paper")))
+            {
+                winner = 2;
+                Console.WriteLine("Computer wins!");
+            } 
+            else    
+            {
+                winner = 0;
+                Console.WriteLine("You did not enter a valid hand. Valid hands include rock, paper, or scissors. Please try again.");
+            }
+
+            //increases score depending on winner
+            if (winner == 1)
+            {
+                yourScore++;
+            }
+            else if (winner == 2)
+            {
+                computerScore++;
+            }
+
+            //displays updated scoreboard
+            Console.WriteLine($"Current Score: Player 1-{yourScore} / Computer-{computerScore}");
+            Console.WriteLine();
+
+
+            //trigger loop to determine if player would like to continue playing game
+            GameLoop(winner, yourScore, computerScore);
         }
         public static void Main()
         {
 
-            //initalize scoreboard variables
+            //initalize variables
             int yourScore = 0;
             int computerScore = 0;
+            int winner = 0;
 
+            Console.WriteLine();
             Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             Console.WriteLine("Welcome to Rock Paper Scissors!");
             Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-            GamePlay();
-            Scoreboard(yourScore, computerScore);
+            GamePlay(winner, yourScore, computerScore);
 
             // leave this command at the end so your program does not close automatically
             // Console.ReadLine();
