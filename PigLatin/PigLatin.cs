572e2018d550059583b63b12207e47cc2fef7474
lisawiththegoodcode <lisa.umhey@gmail.com>
2018-07-23 13:50:03 -0500

    pig latin ready for grading

--- PigLatin/PigLatin.cs
+++ PigLatin/PigLatin.cs
@@ -48,56 +48,112 @@ namespace PigLatin
             // Console.WriteLine(translatedWord);
             // GameLoop();
         }
-        // public static void Output(string )
-        public static void Read(string input)
+
+        public static string TranslatorPrep(string readThis)
         {
-            //initialize arrays
-            string[] words = input.Split(' '); //break sentences into words
-            string[] translation = new string [words.Length]; //initializes a translation array that will contain as many words as the input
-            // Console.WriteLine("Here are my words:" + words.Length);
-
-            for (int i = 0; i < words.Length; i++)
+            //initialize variables, with two vowel char arrays to distinguish between when they are first letter of the word or later in the word
+            string word = "";
+            char[] firstLetterVowels = { 'A', 'E', 'I', 'O', 'U' };
+            char[] vowels = { 'A', 'E', 'I', 'O', 'U', 'Y' };     
+            //begin if-else to determine type of word and send it to the appropriate converter
+            //does the word start with vowel aeiou?
+            if (readThis.IndexOfAny(firstLetterVowels) == 0)
             {
-                //initialize variables, distinguishing between vowels when they are first letter of the word or later in the word
-                char[] firstLetterVowels = { 'A', 'E', 'I', 'O', 'U' };
-                char[] vowels = { 'A', 'E', 'I', 'O', 'U', 'Y' };          
-                string readThis = words[i];
-                string word = "";    
-                // Console.WriteLine("In for loop, here's my readThis:" + readThis);
-
-                //begin if-else to determine type of word and send it to the appropriate converter
-                //does the word start with vowel aeiou?
-                if (readThis.IndexOfAny(firstLetterVowels) == 0)
+                word = VowelTranslator(readThis);
+            } 
+            //if not, it must start with a consonant. But now I want to know, does it have vowel in it at all?
+            else if (readThis.IndexOfAny(vowels) != -1)
+            {
+                //return the position of the first vowel and trigger the translator
+                int firstVowel;
+                if (readThis.IndexOfAny(vowels) == 0)
                 {
-                    word = VowelTranslator(readThis);
-                } 
-                //if not, it must start with a consonant. But now I want to know, does it have vowel in it at all?
-                else if (readThis.IndexOfAny(vowels) != -1)
-                {
-                    //return the position of the first vowel and trigger the translator
-                    int firstVowel;
-                    if (readThis.IndexOfAny(vowels) == 0)
-                    {
-                        //if the first letter is a y, then it's not actually a vowel, so chop it off and try again to find the real first vowel
-                        string yAsFirst = readThis.Substring(0,1);
-                        string next = readThis.Substring(1);
-                        firstVowel = (next.IndexOfAny(vowels)) + 1;
-                    }
-                    else
-                    {
-                        firstVowel = readThis.IndexOfAny(vowels);
-                    }
-                    word = ConsonantTranslator(readThis, firstVowel);
-                }  
-                //does not contain a vowel, execute NoVowelTranslator
+                    //if the first letter is a y, then it's not actually a vowel, so chop it off and try again to find the real first vowel
+                    string yAsFirst = readThis.Substring(0,1);
+                    string next = readThis.Substring(1);
+                    firstVowel = (next.IndexOfAny(vowels)) + 1;
+                }
                 else
                 {
-                    word = NoVowelTranslator(readThis);
+                    firstVowel = readThis.IndexOfAny(vowels);
                 }
+                word = ConsonantTranslator(readThis, firstVowel);
+            }  
+            //does not contain a vowel, execute NoVowelTranslator
+            else
+            {
+                word = NoVowelTranslator(readThis);
+            }
+            return word;
+        }
+        // public static void Output(string)
+        public static void Read(string input)
+        {
+            //initialize input and output arrays, string variables for each individual words
+            string[] words = input.Split(' '); //break sentences into words
+            string[] translation = new string [words.Length]; //initializes a translation array that will contain as many words as the input
+            string word = "";
 
-                //add translated word into the translation array
+            //begin iteration through each word
+            for (int i = 0; i < words.Length; i++)
+            {
+                string readThis = words[i];
+                string keepThisStart = "";
+                string keepThisEnd = "";
+                // Console.WriteLine("In for loop, here's my readThis:" + readThis);
+                
+                //iterate through characters of each word to search for punctuation
+                var chars = readThis.ToCharArray();
+                char[] punctuation = {'.', ',', '\'', '\"', '?', '!'};
+                for (int y = 0; y < chars.Length; y++)
+                {
+                    //if there is punctuation, figure out where it appears in the string
+                    if (Char.IsPunctuation(chars[y]))
+                    {
+                        // Console.WriteLine($"there's punctuation at position {y}!");
+                        //if it's at the beginning, remove it before sending it to the translator and keep it to add back to the beginning of the word after it gets translated
+                        if (y == 0)
+                        {
+                            // Console.WriteLine("and it's at the beginning of a string");
+                            readThis = readThis.TrimStart(punctuation);
+                            keepThisStart = (chars[y]).ToString();
+                            // Console.WriteLine($"new word: {readThis}, punctuation: {keepThisStart}");
+                        } 
+
+                        //if it's at the end, remove it before sending it to the translator and keep it to add back to the end of the word after it gets translated
+                        if (y == ((chars.Length) - 1))
+                        {
+                            // Console.WriteLine("and it's at the end of the string");
+                            readThis = readThis.TrimEnd(punctuation);
+                            keepThisEnd = (chars[y]).ToString();
+                            // Console.WriteLine($"new word: {readThis}, punctuation: {keepThisEnd}");
+
+                        }
+
+                        //if it's in the middle I suppose we should just remove it? ex that's would be atsthay not at'sthay
+                        if (y != 0 && (y != (chars.Length - 1)))
+                        {
+                            int position = y;
+                            readThis = readThis.Remove(y,1);
+                            // Console.WriteLine("and it's somewhere in the middle of the string");
+                            
+                        }
+                    }
+                    //if we have something to add to the start or end of this word, translate it first then add it back in
+                    if (keepThisStart != "" || keepThisEnd != "")
+                    {
+                        //prepend/append any punctuation that was on the front or back of the word
+                        word = TranslatorPrep (readThis);
+                        word = keepThisStart + word + keepThisEnd;
+                    }
+                    //otherwise do go through the translation process for the word
+                    else
+                        word = TranslatorPrep (readThis);
+                }         
+                //add each translated word into the translation array
                 translation[i] = word;
             }
+            //join the translation array and pring the result to the screen
             string output = String.Join(" ", translation);
             Console.WriteLine(output);
             GameLoop();
