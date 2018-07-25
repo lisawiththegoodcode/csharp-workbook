da2d375133d8166646750d9aa0bff64728c64e25
lisawiththegoodcode <lisa.umhey@gmail.com>
2018-07-25 16:25:47 -0500

    one solution with for loop. will write a solution with each of the other loops as well

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -6,7 +6,32 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to FizzBuzz! To what number shall we count?");
+            int countTo = Int32.Parse(Console.ReadLine());
+            ForLoop(countTo);
         }
+        static void ForLoop(int countTo)
+        {
+            for (int i = 1; i <= countTo; i++)
+            {
+                if (i%15 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                } 
+                else if (i%3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                } 
+                else if (i%5 ==0)
+                {
+                    Console.WriteLine("buzz");
+                } 
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
     }
 }
