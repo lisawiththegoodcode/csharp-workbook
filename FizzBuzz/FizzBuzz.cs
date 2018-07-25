c5e00d72cbf2865ae5b3dc4190d15ee2db402079
lisawiththegoodcode <lisa.umhey@gmail.com>
2018-07-25 17:58:50 -0500

    pre class fizz buzz

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -9,6 +9,9 @@ namespace FizzBuzz
             Console.WriteLine("Welcome to FizzBuzz! To what number shall we count?");
             int countTo = Int32.Parse(Console.ReadLine());
             ForLoop(countTo);
+            ForEachLoop(countTo);
+            WhileLoop(countTo);
+            DoWhileLoop(countTo);
         }
         static void ForLoop(int countTo)
         {
@@ -33,5 +36,80 @@ namespace FizzBuzz
             }
         }
 
+        public static void WhileLoop(int countTo)
+        {
+            int i = 1;
+            while (i<=countTo)
+            {
+                if (i%15 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                } 
+                else if (i%3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                } 
+                else if (i%5 ==0)
+                {
+                    Console.WriteLine("buzz");
+                } 
+                else
+                {
+                    Console.WriteLine(i);
+                }
+                i++;
+            }
+        }
+        public static void DoWhileLoop (int countTo)
+        {
+            int i = 1;
+            do
+            {
+                if (i%15 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                } 
+                else if (i%3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                } 
+                else if (i%5 ==0)
+                {
+                    Console.WriteLine("buzz");
+                } 
+                else
+                {
+                    Console.WriteLine(i);
+                }
+                i++;
+            } while (i<=countTo);
+        }
+        public static void ForEachLoop(int countTo)
+        {
+            int[] nums = new int[countTo];
+
+            foreach (int i in nums)
+            {
+                nums[i] = i
+
+
+            }
+                            if (i%15 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                } 
+                else if (i%3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                } 
+                else if (i%5 ==0)
+                {
+                    Console.WriteLine("buzz");
+                } 
+                else
+                {
+                    Console.WriteLine(i);
+                }
+        }
     }
 }
