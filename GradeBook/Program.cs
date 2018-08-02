6c69d1b1b7f2d095b0283710110f55301cf299e5
lisawiththegoodcode <lisa.umhey@gmail.com>
2018-08-02 17:24:49 -0500

    gradebook ready for grading

--- GradeBook/Program.cs
+++ GradeBook/Program.cs
@@ -8,6 +8,7 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            //declare variables
             string nameInput;
             string[] students;
             string gradeInput;
@@ -17,30 +18,35 @@ namespace GradeBook
             Dictionary<string, int[]> gradeBook = new Dictionary<string, int[]>();
 
             Console.WriteLine(@"Please enter your list of students. Use a comma and a single space to seperate each student's name (i.e. Lisa, Jason).");
-            // Type ""done"" when you are finished entering students.
             nameInput = Console.ReadLine();
             students = nameInput.Split(", ");
 
+            //loop through each student inputted and add them to a stack "my students." 
             foreach (string student in students)
             {
                 myStudents.Push(student);
-                Console.WriteLine("Please enter the grades for {0}", student);
+
+                // prompt for grade data, split this input into individual strings for each grade 
+                Console.WriteLine($"Please enter the grades for {student}. Use a comma and a single space to seperate each grade.");
                 gradeInput = Console.ReadLine();
                 stringGrades = gradeInput.Split(", ");
+
+                //initialize int array "intGrades" to store grades entered. convert each grade to an integer and then store it in the int array 
                 intGrades = new int[stringGrades.Length];
-
-
                 for (int i = 0; i < stringGrades.Length; i++) 
                 {
                     intGrades[i] = Int32.Parse(stringGrades[i]);
                     // Console.WriteLine(stringGrades[i]);
                 }  
                 
+                //initialize a dictionary with student name as key and grades (as int array) as values. 
                 gradeBook.Add(student, intGrades);
-                // double average = intGrades.Average();
-                // Console.WriteLine(average);
             }
 
+            //Use stack to print student count and confirm last entry. 
+            Console.WriteLine($"You entered grades for {myStudents.Count} students. The last student you entered a grade for was {myStudents.Peek()}. Here are the current GPAs for each student:");
+
+            //calculate GPA for each student and their array of grades
             foreach (var name in gradeBook.Keys)
             {
                 int[] grades = gradeBook[name];
