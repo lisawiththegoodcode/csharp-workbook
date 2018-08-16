c5c9f1dca7ffecd293f504b1a8b90b2b4ad6d19d
lisawiththegoodcode <lisa.umhey@gmail.com>
2018-08-16 14:39:27 -0500

    mastermind game complete

--- Mastermind/Mastermind.cs
+++ Mastermind/Mastermind.cs
@@ -1,120 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
 
-// namespace Mastermind
-// {
-//     class Program
-//     {
-//         // possible letters in code
-//         public static char[] letters = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' };
-        
-//         // size of code
-//         public static int codeSize = 4;
-        
-//         // number of allowed attempts to crack the code
-//         public static int allowedAttempts = 10;
-        
-//         // number of tried guesses
-//         public static int numTry = 0;
-        
-//         // test solution
-//         public static char[] solution = new char[] {'a', 'b', 'c', 'd'};
-        
-//         // game board
-//         public static string[][] board = new string[allowedAttempts][];
-        
-        
-//         public static void Main()
-//         {
-//             char[] guess = new char[4];
-
-//             CreateBoard();
-//             DrawBoard();
-//             Console.WriteLine("Enter Guess:");
-//             guess = Console.ReadLine().ToCharArray();
-
-//             // leave this command at the end so your program does not close automatically
-//             Console.ReadLine();
-//         }
-        
-//         public static bool CheckSolution(char[] guess)
-//         {
-//             // Your code here
-
-//             return false;
-//         }
-        
-//         public static string GenerateHint(char[] guess)
-//         {
-//             // Your code here
-//             return " ";
-//         }
-        
-//         public static void InsertCode(char[] guess)
-//         {
-//             // Your code here
-//         }
-        
-//         public static void CreateBoard()
-//         {
-//             for (var i = 0; i < allowedAttempts; i++)
-//             {
-//                 board[i] = new string[codeSize + 1];
-//                 for (var j = 0; j < codeSize + 1; j++)
-//                 {
-//                     board[i][j] = " ";
-//                 }
-//             }
-//         }
-        
-//         public static void DrawBoard()
-//         {
-//             for (var i = 0; i < board.Length; i++)
-//             {
-//                 Console.WriteLine("|" + String.Join("|", board[i]));
-//             }
-            
-//         }
-        
-//         public static void GenerateRandomCode() {
-//             Random rnd = new Random();
-//             for(var i = 0; i < codeSize; i++)
-//             {
-//                 solution[i] = letters[rnd.Next(0, letters.Length)];
-//             }
-//         }
-//     }
-// }
-
-
 namespace Mastermind {
     class Program {
+        //added code to randomly generate solution
+        public static string[] letters = new string[] { "a", "b", "c", "d", "e", "f"};
+        public static int codeSize = 4;
+        public static string[] solution = new string[] {"a", "b", "c", "d"};
+
+        public static void GenerateRandomCode() 
+        {
+            Random rnd = new Random();
+            for(var i = 0; i < codeSize; i++)
+            {
+                solution[i] = letters[rnd.Next(0, letters.Length)];
+            }
+        }
         static void Main (string[] args) {
-            Game game = new Game (new string[] { "a", "b", "c", "d" });
-            for (int turns = 10; turns > 0; turns--) {
+            //added code to pass maximum number of tries into game
+            int maxTries = 10;
+            //generates random solution; can comment this out to run with test solution "abcd"
+            GenerateRandomCode();
+            Game game = new Game (maxTries, solution);
+            int turns;
+            for (turns = maxTries; turns > 0; turns--)
+            {
                 Console.WriteLine($"You have {turns} tries left");
                 Console.WriteLine ("Choose four letters: ");
                 string letters = Console.ReadLine ();
                 Ball[] balls = new Ball[4];
 
-                for (int i = 0; i < 4; i++) {
+                for (int i = 0; i < 4; i++) 
+                {
                     balls[i] = new Ball (letters[i].ToString());
                 }
                 Row row = new Row (balls);
                 game.AddRow (row);
                 
-                Console.WriteLine (game.Rows);
+                //code added to tell user they won and end the game
+                if (game.Rows == "win")
+                {
+                    Console.WriteLine(" - that's right!");
+                    break;
+                }
             }
-            Console.WriteLine ("Out Of Turns");
+            if (turns == 0)
+            {
+                Console.WriteLine ("Game Over: Out Of Turns");
+            }
+            else
+            {
+                Console.WriteLine ("Game Over: You Won");
+            }
         }
     }
 
     class Game {
         private List<Row> rows = new List<Row> ();
         private string[] answer = new string[4];
+        private int maxTries;
 
-        public Game (string[] answer) {
+        public Game (int maxTries, string[] answer) {
             this.answer = answer;
+            this.maxTries = maxTries;
         }
 
         private string Score (Row row) {
@@ -140,15 +88,7 @@ namespace Mastermind {
                 }
             }
             
-            if (red == 4)
-            {
-                result = " - That's correct, you won!";
-            }
-            else
-            {
-                result = $" {red} - {white - red}";
-
-            } 
+            result = $" {red} - {white - red}";
 
             return result;
         }
@@ -159,11 +99,21 @@ namespace Mastermind {
 
         public string Rows {
             get {
+                string result = "";
+
                 foreach (var row in this.rows) {
                     Console.Write (row.Balls);
-                    Console.WriteLine (Score (row));
+                    //code added to determine if there was a win
+                    if ((Score(row)) == " 4 - 0")
+                    {
+                        result = "win";
+                    }
+                    else    
+                    {
+                        Console.WriteLine (Score (row));
+                    }
                 }
-            return "";
+            return result;
             }
         }
     }
