76260d0de9cdfe5c2d93929b7cb38e7e80b7d184
lisawiththegoodcode <lisa.umhey@gmail.com>
2018-08-27 15:29:53 -0500

    first commit

--- Checkers/Checkers.cs
+++ Checkers/Checkers.cs
@@ -8,19 +8,33 @@ namespace Checkers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Checker black = new Checker(Color.black);
+            // Checker white = new Checker(Color.white);
+            // Console.WriteLine("Black Checker: " + black.Symbol);
+            // Console.WriteLine("White Checker: " + white.Symbol);
+            Game g1 = new Game();
+
         }
     }
 
+    public enum Color { black, white };
+
     public class Checker
     {
-        public string Symbol  { get; set; }
-        public int[] Position  { get; set; }
-        public string Color { get; set; }
+        public string Symbol  { get; private set; }
+
         
-        public Checker(string color, int[] position)
+        public Checker(Color color)
         {
-            // Your code here
+            if (color == Color.black)
+            {  
+                Symbol = "•"; 
+
+            }
+            else if (color == Color. white)
+            {
+                Symbol = "◦";
+            }
         }
     }
 
@@ -29,22 +43,29 @@ namespace Checkers
         public string[][] Grid  { get; set; }
         public List<Checker> Checkers { get; set; }
         
+        //board constructor initializes checkers and new gameboard
         public Board()
         {
-            // Your code here
-            return;
-        }
-        
-        public void CreateBoard()
-        {
-            // Your code here
-            return;
-        }
-        
-        public void GenerateCheckers()
-        {
-            // Your code here
-            return;
+            Checkers = new List<Checker>();
+            
+            for(int i=0; i<12; i++)
+            {
+                Checkers.Add(new Checker(Color.black));
+                Checkers.Add(new Checker(Color.white));
+            }
+            Console.WriteLine(Checkers.Count + "checkers have been instantiated");
+
+            Grid = new string[][] 
+            {
+                new string[] {" ", " ", " ", " ", " ", " ", " ", " "},
+                new string[] {" ", " ", " ", " ", " ", " ", " ", " "},
+                new string[] {" ", " ", " ", " ", " ", " ", " ", " "},
+                new string[] {" ", " ", " ", " ", " ", " ", " ", " "},
+                new string[] {" ", " ", " ", " ", " ", " ", " ", " "},
+                new string[] {" ", " ", " ", " ", " ", " ", " ", " "},          
+                new string[] {" ", " ", " ", " ", " ", " ", " ", " "},
+                new string[] {" ", " ", " ", " ", " ", " ", " ", " "}
+            };
         }
         
         public void PlaceCheckers()
@@ -55,14 +76,28 @@ namespace Checkers
         
         public void DrawBoard()
         {
-            // Your code here
-            return;
+            Console.WriteLine("  0 1 2 3 4 5 6 7");
+            Console.WriteLine("0 " + String.Join("|", Grid[0]));
+            Console.WriteLine("  ---------------");
+            Console.WriteLine("1 " + String.Join("|", Grid[1]));
+            Console.WriteLine("  ---------------");
+            Console.WriteLine("2 " + String.Join("|", Grid[2]));
+            Console.WriteLine("  ---------------");
+            Console.WriteLine("3 " + String.Join("|", Grid[3]));
+            Console.WriteLine("  ---------------");
+            Console.WriteLine("4 " + String.Join("|", Grid[4]));
+            Console.WriteLine("  ---------------");
+            Console.WriteLine("5 " + String.Join("|", Grid[5]));
+            Console.WriteLine("  ---------------");
+            Console.WriteLine("6 " + String.Join("|", Grid[6]));
+            Console.WriteLine("  ---------------");
+            Console.WriteLine("7 " + String.Join("|", Grid[7]));
         }
         
-        public Checker SelectChecker(int row, int column)
-        {
-            return Checkers.Find(x => x.Position.SequenceEqual(new List<int> { row, column }));
-        }
+        // public Checker SelectChecker(int row, int column)
+        // {
+        //     return Checkers.Find(x => x.Position.SequenceEqual(new List<int> { row, column }));
+        // }
         
         public void RemoveChecker(int row, int column)
         {
@@ -70,17 +105,18 @@ namespace Checkers
             return;
         }
         
-        public bool CheckForWin()
-        {
-            return Checkers.All(x => x.Color == "white") || !Checkers.Exists(x => x.Color == "white");
-        }
+        // public bool CheckForWin()
+        // {
+        //     return Checkers.All(x => x.Color == "white") || !Checkers.Exists(x => x.Color == "white");
+        // }
     }
 
     class Game
     {
         public Game()
         {
-            // Your code here
+            Board b1 = new Board(); 
+            b1.DrawBoard();
         }
     }
 }
